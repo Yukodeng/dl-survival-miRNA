cbf06edcfd0459920163a49b60aedf09e32c9678
dengy <dengy@mskcc.org>
2025-07-01 10:04:21 -0400

    update 0701

--- simulation/batch_effect/augment-parametric simulation/augmented-data-exploration.R
+++
@@ -1,7 +0,0 @@
-
-setwd("~/dl-survival-miRNA/simulation/batch_effect/with_without_batch_maf/")
-data = mxf
-data_batch = read.csv('batch_10_epochES_batch01_maf_generated.csv')
-
-hist(data$hsa.let.7a..2..1)
-hist(data_batch$hsa.let.7a..2..1)
